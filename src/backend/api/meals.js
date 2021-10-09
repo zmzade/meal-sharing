6a3385d62fe1e62ca3f1fa5441db1cf047ee51f2
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-09 21:25:40 +0200

    files

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -5,8 +5,8 @@ const knex = require("../database");
 router.get("/", async (request, response) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meal").select("title");
-    response.json(titles);
+    const meals = await knex("meal").select("*");
+    response.json(meals);
   } catch (error) {
     throw error;
   }
--- src/client/MealId.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const MealId = (props) => {
-  return <div className="add-meal"> id </div>;
-};
-export default MealId;
