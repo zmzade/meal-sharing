36a592ee5240ffdf0e29dc8f92784f0d821ae072
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-09 21:07:10 +0200

    form adding meal

---
+++ src/client/components/MealsChildren.js
@@ -0,0 +1,6 @@
+import React from "react";
+const MealsChildren = (props) => {
+  const { children } = props;
+  return <div className="children">{children}</div>;
+};
+export default MealsChildren;
---
+++ src/client/components/MealsForm.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import postData from "./postData";
+
+const MealsForm = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [when, setWhen] = useState("");
+  const [max_reservations, setMax_reservations] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [created_date, setCreated_date] = useState("");
+
+  const handleAdd = () => {
+    if (!title) {
+      return alert("add title");
+    }
+    if (!description) {
+      return alert("add description");
+    }
+
+    if (!location) {
+      return alert("add location");
+    }
+    if (!max_reservations) {
+      return alert("add maximun reservation");
+    }
+    if (!price) {
+      return alert("add price");
+    }
+    const newMeal = {
+      title: title,
+      description: description,
+      location: location,
+      when: when,
+      max_reservations: max_reservations,
+      price: price,
+      created_date: created_date,
+    };
+    console.log(newMeal);
+    const response = postData("/api/meals", newMeal).then((data) => {
+      console.log(data);
+      if (response) {
+        return alert("data sent");
+      }
+      return alert("there is an error");
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <form>
+          <h2>Add your own food </h2>
+          <div>
+            <label>Title</label>
+            <input
+              type="text"
+              placeholder="Add title .."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Description</label>
+            <input
+              type="text"
+              placeholder="Add description .."
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Location</label>
+            <input
+              type="text"
+              placeholder="Add location .."
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>When</label>
+            <input
+              type="date"
+              placeholder="Add date .."
+              value={when}
+              onChange={(e) => setWhen(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Max-reservation</label>
+            <input
+              type="number"
+              placeholder="Add max reservation .."
+              value={max_reservations}
+              onChange={(e) => setMax_reservations(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Price</label>
+            <input
+              type="number"
+              placeholder="Add price .."
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Created-date</label>
+            <input
+              type="date"
+              placeholder="Add date .."
+              value={created_date}
+              onChange={(e) => setCreated_date(e.target.value)}
+            />
+          </div>
+          <br />
+          <div>
+            <button onClick={handleAdd} type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default MealsForm;
