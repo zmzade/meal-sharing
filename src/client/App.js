155b055e5710c01fd2471feaa56e1059881f1d13
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-09 21:15:36 +0200

    app comp.

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
-import Home from "./Home";
-import Meals from "./Meals";
-import MealId from "./MealId";
+import Meals from "./components/Meals";
+import Home from "./components/Home";
+import MealWithId from "./components/MealWithId";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   const [meals, setMeals] = useState([]);
@@ -12,28 +14,37 @@ function App() {
     fetch("api/meals")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setMeals(data);
       });
   }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/meals/:id">
-          <MealId />
-        </Route>
-        <Route exact path="/meals">
-          <Meals meals={meals} />
-        </Route>
-        <Route exact path="/">
-          <Home meals={meals} />
-        </Route>
-        <Route exact path="/test-component">
-          <TestComponent></TestComponent>
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route exact path="/meals/:id">
+            <MealWithId meals={meals} />
+          </Route>
+          <Route exact path="/meals">
+            <Meals meals={meals} />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home meals={meals} />
+          </Route>
+          <Route exact path="*">
+            <h3>Not Found</h3>
+          </Route>
+
+          <Route exact path="/test-component">
+            <TestComponent></TestComponent>
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
