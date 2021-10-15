9560927eb80aefc8c6e4a6c71d2a62d1bbe004dd
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-15 15:22:42 +0200

    MealWithId comp.

--- src/client/components/MealWithId.js
+++ src/client/components/MealWithId.js
@@ -1,42 +1,93 @@
 import React from "react";
-import { useParams } from "react-router";
 import MealsChildren from "./MealsChildren";
 import ReservForm from "./ReservForm";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-const MealWithId = (props) => {
-  const { meals } = props;
-
+const MealWithId = ({ meals }) => {
+  const [availbleReserv, setAvailableReserv] = React.useState([]);
+  const [isAvailable, setIsAvailable] = React.useState(true);
   const params = useParams();
+
+  React.useEffect(() => {
+    fetch(`/api/meals?availableReservations=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAvailableReserv(data);
+      });
+  }, []);
+
+  const showReservForm = () => {
+    const availableIdArr = availbleReserv.map((food) => food.id);
+    if (availableIdArr.includes(parseInt(params.id))) {
+      console.log(isAvailable);
+      return setIsAvailable(isAvailable);
+    }
+    return console.log(!isAvailable);
+  };
+
+  const mealsWithId = meals.filter((meal) => meal.id === parseInt(params.id));
+  if (mealsWithId.length === 0) {
+    return (
+      <div className="add-meal">
+        {/* <div>
+          <ReservForm />
+        </div> */}
+      </div>
+    );
+  }
+  const meal = mealsWithId[0];
 
   return (
     <div className="add-meal">
       <div>
-        {meals
-          .filter((meal) => meal.id === parseInt(params.id))
-          .map((meal) => (
-            <div key={meal.id}>
-              <div style={{ fontSize: "20px", color: "darkblue" }}>
-                {meal.title}
-              </div>
-              <br />
-              <MealsChildren>
-                Id: {params.id}
-                <br />
-                Description: {meal.description}
-                <br />
-                Price: {meal.price}
-                <br />
-                Location: {meal.location}
-                <br />
-                Max-reservation: {meal.max_reservations}
-              </MealsChildren>
-              <br />
-              <MealsChildren>created-date: {meal.created_date}</MealsChildren>
-            </div>
-          ))}
+        <div style={{ fontSize: "20px" }}>
+          <h3>{meal.title}</h3>
+        </div>
+        <br />
+        <MealsChildren>
+          Id: {params.id}
+          <br />
+          Ingredients: {meal.description}
+          <br />
+          Price: {meal.price} Kr.
+          <br />
+          Max-reservation: {meal.max_reservations}
+          <br />
+          Where: {meal.location}
+          <br />
+          when: {new Date(meal.created_date).toLocaleDateString()}
+        </MealsChildren>
+
+        <br />
+        <div>
+          {" "}
+          <input type="button" onClick={showReservForm} value="reserve"></input>
+        </div>
+        <br />
+        <div>
+          <Link to={`/reviews/${meal.id}`}>
+            <button
+              style={{
+                backgroundColor: "whitesmoke",
+                width: "100%",
+                marginLeft: "0px",
+              }}
+            >
+              leave a review for this meal
+            </button>
+          </Link>
+        </div>
       </div>
+
       <div>
-        <ReservForm mealId={parseInt(params.id)} />
+        {isAvailable ? (
+          <ReservForm />
+        ) : (
+          <div>
+            <h4>Unfortunately, no available reservation</h4>
+          </div>
+        )}
       </div>
     </div>
   );
