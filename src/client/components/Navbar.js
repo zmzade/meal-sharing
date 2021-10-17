ae291bcc249e377c9de506f2fd869e64d7219123
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-17 23:07:58 +0200

    Navbar comp edited

--- src/client/components/Navbar.js
+++ src/client/components/Navbar.js
@@ -1,35 +1,65 @@
 import React from "react";
 import { MdOutlineFoodBank } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
       <div className="navbar">
-        <div>
+        {/* <div>
           <img
             src="https://upload.wikimedia.org/wikipedia/en/e/ec/Yogurt_Mountain_logo.png"
             alt="fruits"
             width="40"
             height="30"
           />
-        </div>
+        </div> */}
         <div>
           <ul>
-            <li className="nav-item">
-              {/* <MdOutlineFoodBank size={50} /> */}
+            <li className="logo">
+              <MdOutlineFoodBank size={50} />
             </li>
 
             <li className="nav-item">
-              <Link to="/"> home</Link>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="navbar-active"
+                className="main-nav"
+              >
+                {" "}
+                home
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/meals"> share meal</Link>
+              <NavLink
+                exact
+                to="/meals"
+                activeClassName="navbar-active"
+                className="main-nav"
+              >
+                {" "}
+                share meal
+              </NavLink>
             </li>
-            {/* <li className="nav-item">
-              <Link to="/meals/:id">reservation</Link>
-            </li> */}
             <li className="nav-item">
-              <Link to="/about">about</Link>
+              <NavLink
+                exact
+                to="/meals/:id"
+                activeClassName="navbar-active"
+                className="main-nav"
+              >
+                reservation
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="navbar-active"
+                className="main-nav"
+              >
+                about
+              </NavLink>
             </li>
           </ul>
         </div>
