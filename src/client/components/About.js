f535bc890bc92a084964c6f5b20b31d088fbf239
zmzade <zmahmoodzadeh@yahoo.com>
2021-10-17 23:04:38 +0200

    about comp. edited

--- src/client/components/About.js
+++ src/client/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 const About = () => {
   return (
-    <div className="add-meal">
-      <h3>our taste</h3>
+    <div className="about">
+      <div>
+        <h3>our taste</h3>
+      </div>
+      <div className="aboutDiv">
+        We recently created this website for sharing meal. Share your food and
+        leave your reviews for thousands of suscribers.
+      </div>
     </div>
   );
 };
